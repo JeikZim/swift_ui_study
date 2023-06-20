30a60b8d3aaab9da8cdcf6c47f93516a370678c7
JeikZim <bagautdinovwruslan@gmail.com>
2023-06-20 13:40:12 +0300

    Initial Commit

---
+++ studyFirst/studyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  studyFirstApp.swift
+//  studyFirst
+//
+//  Created by Гость on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct studyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
