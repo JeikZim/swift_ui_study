c1406cd69639a715e64e73ea72101e4648a7f6db
JeikZim <bagautdinovwruslan@gmail.com>
2023-06-21 10:36:16 +0300

    mid

--- studyFirst/ContentView.swift
+++ studyFirst/ContentView.swift
@@ -8,45 +8,104 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = Array(["🛶", "✈️", "🚁", "🚀", "🚀", "🛶", "✈️", "🚁", "🚀", "🚀","🛶", "✈️", "🚁", "🚀", "🚀"].enumerated())
+    @State var emonjiCount = 4
+    
     var body: some View {
-        ZStack {
-            VStack(alignment: .center) {
-                HStack {
-                    GlobeImage()
-                    GlobeImage()
-                    GlobeImage()
+        var addButton: some View = StdButton(action: {
+            emonjiCount += 1
+        }, title: "+")
+        var removeButton: some View = StdButton(action: {
+            emonjiCount -= 1
+        }, title: "-")
+        
+        VStack {
+            HStack {
+                ForEach(emojis[0..<emonjiCount], id: \.offset) { emoji in
+                    CardView(content: emoji.element)
+                    
                 }
-                Text("Hello, Pure Native!")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-//                    .foregroundColor(.red)
             }
-            .foregroundColor(.orange)
-            RoundedRectangle(cornerRadius: 25)
-                .stroke(lineWidth: 5)
-                .foregroundColor(.orange)
-                .padding(.horizontal)
+            .padding(.all)
+            HStack {
+                addButton
+                Spacer()
+                removeButton
+            }
+            
         }
-        .padding(.vertical)
     }
 }
+
+struct StdButton: View {
+    var action: () -> Void
+    var title: String
+    
+    var body: some View {
+        var textColor: Color = Color.white
+        var bgColor: Color = Color.blue
+        
+        Button(action: action, label: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 6)
+                    .foregroundColor(bgColor)
+                    .frame( width: 120, height: 40)
+                Text(title)
+                    .foregroundColor(textColor)
+                    .font(.title)
+                    .fontWeight(.bold)
+            }
+            .padding(.horizontal, 5)
+            .padding(.bottom, 5)
+
+        })
+    }
+}
+
+struct CardView: View {
+    let id = UUID()
+    let content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let radius: CGFloat = 16
+            let outerColor: Color = Color.orange
+            let innerColor: Color = Color.white
+            
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: radius)
+                    .fill()
+                    .foregroundColor(innerColor)
+                RoundedRectangle(cornerRadius: radius)
+                    .stroke(lineWidth: 3)
+                    .foregroundColor(outerColor)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: radius)
+                    .fill()
+                    .foregroundColor(outerColor)
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-//func GlobeImage () -> some View {
-//    return Image(systemName: "globe")
-//        .imageScale(.large)
-//        .foregroundColor(.accentColor)
-//}
-
-struct GlobeImage: View {
-    var body: some View {
-        Image(systemName: "globe")
-            .imageScale(.large)
-            .foregroundColor(.accentColor)
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
+            
     }
 }
